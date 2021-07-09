755dcb0e37f652116b9cd95b6a6a3ab39c65aea1
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-09 15:20:21 +0530

    [Soubarnika]Printing Array  Using Generics Class

--- Generics/PrintArray.cs
+++ Generics/PrintArray.cs
@@ -6,33 +6,24 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    class PrintArray
-    {
-        //Printing the array using generic type T
-        public static void ToPrintArray<T>(T[] inputArray)
+        class PrintArray<T>
         {
-            Console.WriteLine("======Printing Array======");
-            foreach (var i in inputArray)
+            public T[] inputArray;
+            public PrintArray(T[] inputArray)
             {
-                Console.WriteLine(i);
+                this.inputArray = inputArray;
             }
+            //Printing the array using generic type T
+            public void ToPrintArray()
+            {
+                Console.WriteLine("======Printing Array======");
+                foreach (var i in this.inputArray)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
         }
-
-
-        public static void ReadInput()
-        {
-            int[] intArray = { 2,6,5,7,833,55,66};
-            double[] doubleArray = { 1.1, 2,5, 5.6, 7.8, 80.5, 1 };
-            char[] charArray = { 'w','e' ,'l','c','o','m','e'};
-            ToPrintArray(intArray);
-            ToPrintArray(doubleArray);
-            ToPrintArray(charArray);
-        }
-
-
-
-
-
-    }
 }
 
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,13 +4,22 @@ namespace Generics
 {
     class Program
     {
+        /// <summary>
+        /// UC1- Printing different array without generic type
+        /// UC2- Printing array using generic type method
+        /// UC3- Printig Array using generic class
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Implementaion of Generics !");
-            Console.WriteLine("Printing Array Without Using Generics");
-
-            PrintArray.ReadInput();
-
+            Console.WriteLine("Printing array of different data type");
+            int[] intArray = { 1,3,6,9 };
+            double[] doubleArray = { 5.5,6.6,8.36, 6.2, 80.5, 1 };
+            char[] charArray = { 'w', 'e', 'l', 'c', 'o', 'm', 'e' };
+            new PrintArray<int>(intArray).ToPrintArray();
+            new PrintArray<double>(doubleArray).ToPrintArray();
+            new PrintArray<char>(charArray).ToPrintArray();
+            Console.Read();
         }
     }
 }
